c85038fb1d11a188d03e9fb745c7879edf911d80
solnor <solve.norsterud@gmail.com>
2023-11-10 14:17:47 +0100

    Update comments

--- assignment 2/task2.py
+++ assignment 2/task2.py
@@ -43,8 +43,8 @@ def create_model():
         nn.ReLU(),
         nn.MaxPool2d(kernel_size=2, stride=2),
         #
-        nn.Flatten(),  # Flattens the image from shape (batch_size, C, Height, width) to (batch_size, C*height*width)
-        nn.Linear(128*4*4, 64),
+        nn.Flatten(),
+        nn.Linear(128*4*4, 64), # Output shape of last MaxPool2d is 4x4 with 128 channels
         nn.ReLU(),
         nn.Linear(64, 10),
         # No need to include softmax, as this is already combined in the loss function
@@ -54,12 +54,12 @@ def create_model():
     return model
 
 
-model = create_model()
+model_SGD = create_model()
 
 
 # Test if the model is able to do a single forward pass
 example_images = utils.to_cuda(example_images)
-output = model(example_images)
+output = model_SGD(example_images)
 print("Output shape:", output.shape)
 expected_shape = (batch_size, 10)  # 10 since mnist has 10 different classes
 assert output.shape == expected_shape,    f"Expected shape: {expected_shape}, but got: {output.shape}"
@@ -67,6 +67,7 @@ assert output.shape == expected_shape,    f"Expected shape: {expected_shape}, bu
 
 # Hyperparameters
 learning_rate = .02
+learning_rate = .001
 num_epochs = 5
 
 
@@ -74,34 +75,56 @@ num_epochs = 5
 loss_function = torch.nn.CrossEntropyLoss()
 
 # Define optimizer (Stochastic Gradient Descent)
-optimizer = torch.optim.SGD(model.parameters(),
+optimizer = torch.optim.SGD(model_SGD.parameters(),
                             lr=learning_rate)
 
 
 trainer = Trainer(
-    model=model,
+    model=model_SGD,
     dataloader_train=dataloader_train,
     dataloader_test=dataloader_test,
     batch_size=batch_size,
     loss_function=loss_function,
     optimizer=optimizer
 )
-train_loss_dict, test_loss_dict = trainer.train(num_epochs)
+train_loss_dict_SGD, test_loss_dict_SGD = trainer.train(num_epochs)
 
+model_Adam = create_model()
+
+# Define optimizer (Adam)
+learning_rate = .001
+optimizer = torch.optim.Adam(model_Adam.parameters(),
+                             lr=learning_rate)
+
+trainer = Trainer(
+    model=model_Adam,
+    dataloader_train=dataloader_train,
+    dataloader_test=dataloader_test,
+    batch_size=batch_size,
+    loss_function=loss_function,
+    optimizer=optimizer
+)
+train_loss_dict_Adam, test_loss_dict_Adam = trainer.train(num_epochs)
 
 # We can now plot the training loss with our utility script
 
 # Plot loss
-utils.plot_loss(train_loss_dict, label="Train Loss")
-utils.plot_loss(test_loss_dict, label="Test Loss")
+utils.plot_loss(train_loss_dict_SGD, label="Train Loss SGD")
+utils.plot_loss(test_loss_dict_SGD, label="Test Loss SGD")
+utils.plot_loss(train_loss_dict_Adam, label="Train Loss Adam")
+utils.plot_loss(test_loss_dict_Adam, label="Test Loss Adam")
 # Limit the y-axis of the plot (The range should not be increased!)
-plt.ylim([0, .5])
+plt.ylim([0, .1])
 plt.legend()
 plt.xlabel("Global Training Step")
 plt.ylabel("Cross Entropy Loss")
 plt.savefig(utils.image_output_dir.joinpath("task2a_plot.png"))
-plt.show()
 
-final_loss, final_acc = utils.compute_loss_and_accuracy(
-    dataloader_test, model, loss_function)
-print(f"Final Test loss: {final_loss}. Final Test accuracy: {final_acc}")
+final_loss_SGD, final_acc_SGD = utils.compute_loss_and_accuracy(
+    dataloader_test, model_SGD, loss_function)
+print(f"Final Test loss SGD: {final_loss_SGD}. Final Test accuracy SGD: {final_acc_SGD}")
+
+final_loss_Adam, final_acc_Adam = utils.compute_loss_and_accuracy(
+    dataloader_test, model_Adam, loss_function)
+print(f"Final Test loss Adam: {final_loss_Adam}. Final Test accuracy Adam: {final_acc_Adam}")
+plt.show()
